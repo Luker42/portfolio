0e5b6bd9610f3ca1acda422207e15ac9c2050522
Lucas Reisch <lucasreisch@lucass-air.mynetworksettings.com>
2024-05-04 17:25:21 -0400

    init

---
+++ app/app/root.tsx
@@ -0,0 +1,23 @@
+import {Links, Meta, Outlet, Scripts} from '@remix-run/react'
+import type {LinksFunction} from '@remix-run/node'
+import stylesheet from './tailwind.css?url'
+
+export const links: LinksFunction = () => [
+  {rel: 'stylesheet', href: stylesheet},
+]
+export default function App() {
+  return (
+    <html>
+      <head>
+        <link rel="icon" href="data:image/x-icon;base64,AA" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1 className="text-3xl font-bold underline">Hello world blahhhhh!</h1>
+        <Outlet />
+        <Scripts />
+      </body>
+    </html>
+  )
+}
---
+++ app/app/routes/portfolio.$id/route.tsx
@@ -0,0 +1,6 @@
+import {useParams} from '@remix-run/react'
+
+export default function PersonalizedPortfolio() {
+  const params = useParams()
+  return <div>Personalized portfolio page with ID{params.id} </div>
+}
