ff2c8245e8657ec52ceef73d5f13ced03ff2e73a
Lucas Reisch <lucasreisch@Lucass-MacBook-Air.local>
2024-05-06 10:45:52 -0400

    removing a word

--- app/app/root.tsx
+++ app/app/root.tsx
@@ -14,7 +14,7 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <h1 className="text-3xl font-bold underline">Hello world blahhhh!</h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <Outlet />
         <Scripts />
       </body>
